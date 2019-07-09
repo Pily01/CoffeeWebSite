b9f97191d9fa2cf0abc0cbebd58293e8b200c34c
Pily <paila@live.com.mx>
2019-07-09 01:27:00 -0500

    added main dishes, dessert, and beverages

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ window.onscroll = function() {
 
 function scrollFunction() {
   if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "0px";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-70px";
   }
 }
